9a048d2cf76ca4d444e4c458aa040c17508e7162
CristianWolfgang <78653099+CristianWolfgang@users.noreply.github.com>
2021-05-11 19:01:42 -0400

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,6 @@ import panelBtn from "./modules/panel_btn.js";
 import searchForm from "./modules/search_form.js";
 import selectCategories from "./modules/select_categories.js";
 import footer from "./modules/footer.js";
-import buster from "../../../buster.js";
 import voiceRecon from "./modules/voice_recognition.js";
 import loader from "./modules/loader.js";
 loader();
@@ -13,7 +12,6 @@ d.addEventListener("DOMContentLoaded",e=>{
 	searchForm();
 	selectCategories();
 	footer();
-	buster();
 });
 
 voiceRecon();
