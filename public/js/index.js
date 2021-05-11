47ae9e3b11560655762c03a6f0546f78b61a13e9
Cristian Wolfgang <CristianWolfgang@gmail.com>
2021-05-11 18:56:09 -0400

    loader_added

--- public/js/index.js
+++ public/js/index.js
@@ -4,13 +4,16 @@ import selectCategories from "./modules/select_categories.js";
 import footer from "./modules/footer.js";
 import buster from "../../../buster.js";
 import voiceRecon from "./modules/voice_recognition.js";
+import loader from "./modules/loader.js";
+loader();
 const d = document;
+
 d.addEventListener("DOMContentLoaded",e=>{
 	panelBtn();
 	searchForm();
 	selectCategories();
 	footer();
 	buster();
-})
+});
 
 voiceRecon();
---
+++ public/js/modules/loader.js
@@ -0,0 +1,15 @@
+const d=document;
+export default function loader(){
+	const $loader = d.createElement("div");
+	$loader.classList.add("loader");
+	$loader.innerHTML = "<img src='./public/images/loader.svg'>";
+	d.body.insertAdjacentElement("afterbegin",$loader);
+	d.body.style.overflowY = "hidden";
+
+	window.addEventListener("load",e=>{
+		setTimeout(()=>{
+			d.body.removeChild($loader);
+			d.body.style.overflowY="auto";
+		},2000);
+	})
+};
